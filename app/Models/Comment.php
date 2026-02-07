f52131a8cb0cb18156db09ed27f07e51231a1161
Iago Machado <iago.machado000@gmail.com>
2026-02-07 16:03:25 -0300

    refactor(domain): add trait HasFactory para conexão entre models e factories

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
     const UPDATED_AT = null;
 
     protected $fillable = [
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'title',
