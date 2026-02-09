841eea69e3b6e8e391c58d2cbe8c8f702766474b
Iago Machado <iago.machado000@gmail.com>
2026-02-09 18:46:39 -0300

    refactor(recipe): ajustando lógica de redirecionamento da rota /

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\RatingController;
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::redirect('/', '/recipes');
 Route::resource('recipes', RecipeController::class)->only(['index', 'show']);
+
 Route::middleware('auth')->prefix('dashboard')->group(function () {
     Route::get('recipes', [RecipeController::class, 'dashboard'])->name('recipes.dashboard');
     Route::resource('recipes', RecipeController::class)->except(['index', 'show']);
