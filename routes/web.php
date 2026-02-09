3ed9174e0a6d87374eb40b8946f87a5a3213031b
Iago Machado <iago.machado000@gmail.com>
2026-02-09 14:18:17 -0300

    feat(recipe): add rotas públicas e privados para CRUD recipe

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('recipes', RecipeController::class)->only(['index', 'show']);
+Route::middleware('auth')->prefix('dashboard')->group(function () {
+    Route::get('recipes', [RecipeController::class, 'dashboard'])->name('recipes.dashboard');
+    Route::resource('recipes', RecipeController::class)->except(['index', 'show']);
+});
